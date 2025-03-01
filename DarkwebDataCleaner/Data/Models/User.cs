bbbee4f068383495a846fe3cdd2c6a3bba85a3d6
Monica Neill <Monica.Neill2@jet2.com>
2025-03-01 15:39:33 +0000

    WIP read and extract data using CSV helper

---
+++ DarkwebDataCleaner/Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace DarkwebDataCleaner.Data.Models;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public DateTime DateRegistered { get; set; }
+    public DateTime LastLogin { get; set; }
+    public string RealName { get; set; }
+    public string Password { get; set; }
+    public string PasswordHash { get; set; }
+    public string EmailAddress { get; set; }
+    public string Gender { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Location { get; set; }
+    public bool ShowOnline { get; set; }
+    public string MemberIp { get; set; }
+    public string SecretQuestion { get; set; }
+    public string SecretAnswer { get; set; }
+}
--- DarkwebDataCleaner/Program.cs
+++ DarkwebDataCleaner/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using DarkwebDataCleaner.Data.Models;
+
+namespace DarkwebDataCleaner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "Data/Models/users.csv";
+
+        // Open the file and read the data.
+        using var reader = new StreamReader(filePath);
+        using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+        // Read the rows
+        var records = csv.GetRecords<User>().ToList();
+
+        foreach (var user in records)
+        {
+            Console.WriteLine($"User ID: {user.UserId}, " +
+                              $"User Name: {user.UserName}, " +
+                              $"Date Registered: {user.DateRegistered}, " +
+                              $"Last Login: {user.LastLogin}, " +
+                              $"Real Name: {user.RealName}, " +
+                              $"Password: {user.Password}, " +
+                              $"Password Hash: {user.PasswordHash}, " +
+                              $"Email Address: {user.EmailAddress}, " +
+                              $"Gender: {user.Gender}, " +
+                              $"Birth Date: {user.BirthDate}, " +
+                              $"Location: {user.Location}, " +
+                              $"Show Online: {user.ShowOnline}, " +
+                              $"Member IP: {user.MemberIp}, " +
+                              $"Secret Question: {user.SecretQuestion}, " +
+                              $"Secret Answer: {user.SecretAnswer}");
+        }
+    }
+}
